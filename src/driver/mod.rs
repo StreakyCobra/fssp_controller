2a42c02bc555412dabc384ae4ef1676af8c72705
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-04-07 10:04:10 +0200

    Handle the case when commands connection is not available

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -6,18 +6,22 @@ use std::{thread, time};
 
 pub mod command;
 
+const WAIT_DURATION_MS: u64 = 100;
+
 pub fn connect_driver(address: &str) -> mpsc::Sender<Command> {
     let (tx, rx) = mpsc::channel();
 
     if let Some(stream) = TcpStream::connect(address).ok() {
         thread::spawn(move || emit(stream, rx));
+    } else {
+        thread::spawn(move || dummy(rx));
     };
 
     return tx;
 }
 
 fn emit(mut stream: TcpStream, rx: mpsc::Receiver<Command>) {
-    let wait_duration = time::Duration::from_millis(100);
+    let wait_duration = time::Duration::from_millis(WAIT_DURATION_MS);
     stream.set_nodelay(true).unwrap();
     loop {
         for received in rx.try_iter() {
@@ -26,6 +30,16 @@ fn emit(mut stream: TcpStream, rx: mpsc::Receiver<Command>) {
                 .write(format!("{}\n", received.to_gcode()).as_bytes())
                 .unwrap();
             stream.flush().unwrap();
+        }
+        thread::sleep(wait_duration);
+    }
+}
+
+fn dummy(rx: mpsc::Receiver<Command>) {
+    let wait_duration = time::Duration::from_millis(WAIT_DURATION_MS);
+    loop {
+        for received in rx.try_iter() {
+            println!("{:?}", received);
         }
         thread::sleep(wait_duration);
     }
